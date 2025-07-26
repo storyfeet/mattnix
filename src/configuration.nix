f906c297b6e0478c010e9d1458534279da369033
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-07-26 16:07:47 +0100

    enable flakes

--- src/configuration.nix
+++ src/configuration.nix
@@ -5,6 +5,7 @@
 { config, pkgs, ... }:
 
 {
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
